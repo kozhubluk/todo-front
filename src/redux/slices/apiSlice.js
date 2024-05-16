8e43ea631816a9bacb29af4175458e38a8fd935a
Anastasia <nastya.awdeeva@yandex.ru>
2024-05-16 19:59:34 +0300

    fix

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.BASE_URL,
+  baseUrl: 'process.env.REACT_APP_BASE_URL',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
