60fa83eae1421f5b4aa65b22d787f39db2ef0af4
Anastasia <nastya.awdeeva@yandex.ru>
2024-05-16 19:35:36 +0300

    add env vars

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import './Wrapper.scss';
 
--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:8080',
+  baseUrl: process.env.BASE_URL,
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
